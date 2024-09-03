14cc63e66cc38f2c231050c9da446da6cc221322
midhunps <midhunps.0@gmail.com>
2024-09-03 15:27:28 +0530

    modified metatag helper

--- Ynotz/EasyAdmin/resources/views/classes.blade.php
+++ Ynotz/EasyAdmin/resources/views/classes.blade.php
@@ -1,1 +1,20 @@
-<input type="hidden" class="w-1/2 w-1/3 w-1/4 w-2/3 w-2/5 w-3/4 w-3/5 w-full flex-grow">
+@php
+    $classes = [
+        'md:w-1/2',
+        'md:w-1/3',
+        'md:w-1/4',
+        'md:w-2/3',
+        'md:w-2/5',
+        'md:w-3/4',
+        'md:w-3/5',
+        'md:w-full',
+        'w-1/2',
+        'w-1/3',
+        'w-1/4',
+        'w-2/3',
+        'w-2/5',
+        'w-3/4',
+        'w-3/5',
+        'w-full ',
+    ]
+@endphp
